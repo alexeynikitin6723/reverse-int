a9a941d5798495cad4c78f38c1c64bbc871cfcbc
Alexey Nikitin <alexey.nikitin6723@gmail.com>
2022-02-16 21:53:41 +0400

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	if (n < 0) n = -n;
+	let str = n.toString();
+	str = str.split('').reverse().join('');
+	return Number(str);
 }
